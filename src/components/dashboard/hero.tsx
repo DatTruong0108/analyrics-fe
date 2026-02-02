f23244b3f8ecd16f342c6008e6305d9ab81c2371
DatTruong0108 <dattantruong.work@gmail.com>
2026-02-02 15:28:54 +0700

    update-UI: hero section in mobile

--- src/components/dashboard/hero.tsx
+++ src/components/dashboard/hero.tsx
@@ -1,12 +1,12 @@
-// src/components/dashboard/Hero.tsx
 export default function Hero() {
     return (
-        <section className="flex flex-col items-center text-center space-y-8 mx-6 animate-fade-in">
-            <div className="relative">
+        <section className="w-full flex flex-col items-center text-center space-y-8 animate-fade-in">
+            <div className="relative w-full">
                 <span className="inline-block px-4 py-1.5 text-[10px] md:text-xs border border-zinc-800 rounded-full text-zinc-400 uppercase tracking-[0.3em] font-medium bg-zinc-950/50 mb-6 shadow-[0_0_15px_rgba(255,255,255,0.05)]">
                     Vượt qua giới hạn của đôi tai
                 </span>
-                <h1 className="text-7xl md:text-9xl font-black tracking-tighter uppercase italic leading-[0.8] text-white">
+
+                <h1 className="text-5xl min-[430px]:text-6xl md:text-9xl font-black tracking-tighter uppercase italic leading-[0.8] text-white">
                     Ana<span className="text-zinc-700">lyrics</span>
                 </h1>
             </div>
